35b378419c66c905f3a4d1f2942f30e062539711
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-08-29 11:00:42 +0300

    Update 1.Students ages.cs

--- Sets and Dictionaries Advanced/Exercises/1.Students ages.cs
+++ Sets and Dictionaries Advanced/Exercises/1.Students ages.cs
@@ -1,1 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace DictionariesAndSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array= new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(array[0]);
+
+            Dictionary<string, int> studentsAges = new Dictionary<string, int>()
+            {
+                {"gogich",30},
+                {"joji",80}
+            };
+
+            studentsAges.Add("Gogi", 38);
+            studentsAges.Add("Doroteq",19);
+
+            Console.WriteLine(studentsAges["Gogi"]);
+
+            if (!studentsAges.ContainsKey("pepi"))
+            {
+                Console.WriteLine("Adding pepi");
+                studentsAges.Add("pepi",13);
+            }
+
+            foreach (var student in studentsAges)
+            {
+                Console.WriteLine($"{student.Key} is {student.Value} old");
+            }
+        }
+    }
+}
 
