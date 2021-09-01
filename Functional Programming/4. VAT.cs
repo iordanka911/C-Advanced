a4b470d2adf2392c56c3bdb68fff8157d80697d5
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-09-01 21:54:08 +0300

    Create 4. VAT.cs

---
+++ Functional Programming/4. VAT.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(Decimal.Parse)
+                .Select(number => number+(number * 0.2m))
+                .ToArray();
+            //numbers = Select(numbers, number => number * 5);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+
+        static decimal[] Select(decimal[] array, Func<decimal, decimal> transformer)
+        {
+            decimal[] newArray = new decimal[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = transformer(array[i]);
+            }
+
+            return newArray;
+        }
+    }
+}
