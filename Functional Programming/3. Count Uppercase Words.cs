7fe9be92b401b5f9286354d9768ce04cd26ca2b9
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-09-01 21:36:40 +0300

    Create 3. Count Uppercase Words.cs

---
+++ Functional Programming/3. Count Uppercase Words.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> filter = text => Char.IsUpper(text[0]);
+            string text = Console.ReadLine();
+            string[] words = text.Split(" ",
+                StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Where(filter).ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
