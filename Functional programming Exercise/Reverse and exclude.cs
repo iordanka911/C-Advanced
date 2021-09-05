725fbf0e3dad7ab9ebb96ae1b6da0baff18568a0
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-09-06 00:57:18 +0300

    Update Reverse and exclude.cs

--- Functional programming Exercise/Reverse and exclude.cs
+++ Functional programming Exercise/Reverse and exclude.cs
@@ -22,3 +22,31 @@ namespace ConsoleApp131
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace ConsoleApp131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+            Func<int, bool> predicate = num => num % n != 0;
+            //numbers = numbers.Where(num => num % n != 0).ToList();
+            //numbers = numbers.Where(num => predicate(num)).ToList();
+            numbers = numbers.Where(predicate).ToList();
+            Action<List<int>> print =
+                nums => Console.WriteLine(string.Join(" ", nums));
+            print(numbers);
+        }
+    }
+}
+
