15a5f9c24057f2df76747941d8cf11e9e2f99e7b
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-09-06 15:30:31 +0300

    Create 8. Custom Comparator.cs

---
+++ Functional programming Exercise/8. Custom Comparator.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace ConsoleApp131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ;
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+           
+            
+            Array.Sort(numbers, (a, b) =>
+            {
+                if (a%2!=0&&b%2==0)
+                {
+                    return 1;
+                }
+                else if (a%2==0&&b%2!=0)
+                {
+                    return -1;
+                }
+                return a.CompareTo(b);
+                
+            });
+            Console.WriteLine(string.Join(" ",numbers));
+
+        }
+        
+    }
+}
