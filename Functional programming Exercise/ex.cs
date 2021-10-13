5da54a6634999cc032094ab8a2b2f03b53b75a80
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-10-13 17:13:27 +0300

    Update ex.cs

--- Functional programming Exercise/ex.cs
+++ Functional programming Exercise/ex.cs
@@ -76,3 +76,80 @@ namespace LinkExamples
 
     }
 }
+
+
+
+///////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalProgramming
+{
+    class Person
+    {
+        public string Name;
+
+        public int Age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                var parts = line.Split(", ");
+                var person = new Person();
+                person.Name = parts[0];
+                person.Age = int.Parse(parts[1]);
+                people.Add(person);
+            }
+            var filterName = Console.ReadLine();
+            var ageToCompareWith = int.Parse(Console.ReadLine());
+
+            Func<Person, bool> filter=p=>true;
+
+            if (filterName=="younger")
+            {
+                filter = p => p.Age < ageToCompareWith;
+
+            }
+            else if (filterName=="older")
+            {
+                filter = p => p.Age >= ageToCompareWith;
+            }
+            else if (filterName=="exaxt")
+            {
+                filter = p => p.Age == ageToCompareWith;
+            }
+
+            var filteredPeople = people.Where(predicate: filter);
+
+            var printName = Console.ReadLine();
+            Func<Person, string> printFunc = p => p.Name + " " + p.Age;
+            if (printName=="name age")
+            {
+                printFunc = p => p.Name + " - " + p.Age;
+            }
+            else if (printName=="name")
+            {
+                printFunc = p => p.Name;
+            }
+            else if (printName=="age")
+            {
+                printFunc = p => p.Age.ToString();
+            }
+            var personAsString = filteredPeople.Select(printFunc);
+            foreach(var str in personAsString)
+            {
+                Console.WriteLine(str);
+            }
+        }
+               
+    }
+}
