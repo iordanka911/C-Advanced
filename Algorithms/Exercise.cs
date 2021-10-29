843b8539cc01160f76bf519cd25b091c2186bf14
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-10-29 16:13:08 +0300

    Create Exercise.cs

---
+++ Algorithms/Exercise.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Algorithms_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        private static void Hello()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
